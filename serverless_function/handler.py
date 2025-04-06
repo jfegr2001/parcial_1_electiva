8c9be4d8975a81d078d6951f4dcb04a5a9d765fa
jfegr2001 <giraldo.juan.7954@eam.edu.co>
2025-04-05 19:09:48 -0500

    Despliegue

--- serverless_function/handler.py
+++
@@ -1,4 +0,0 @@
-def hello(event, context):
-    query_params = event.get("queryStringParameters") or {}
-    name = query_params.get("name", "World")
-    return {"statusCode": 200, "body": f"Hello, {name}!"}
